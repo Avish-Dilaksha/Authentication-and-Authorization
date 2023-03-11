a854110fc0fee4ff232e3ea918124bd956229909
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-11 14:03:17 +0530

    updated user.js model again

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserLogSchema = new mongoose.Schema({
     name: {
@@ -11,7 +13,11 @@ const UserLogSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 3,
-        Maxkength: 15
+        Maxkength: 15,
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 
+            'Please provide valid email address',
+        ]
     },
     password: {
         type: String,
@@ -20,5 +26,16 @@ const UserLogSchema = new mongoose.Schema({
         Maxkength: 15
     },
 })
+
+UserSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
+    return isMatch
+}
 
 module.exports = mongoose.model("UsersList", UserLogSchema)
